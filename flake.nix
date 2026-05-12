48bc697042ba3003726942cb98b663f51796f06c
desmond.wang <desmond.wang@netint.ca>
2026-05-12 10:38:24 -0400

    [fix] cleanup the reference

--- flake.nix
+++ flake.nix
@@ -3,7 +3,9 @@
 
   nixConfig = {
     extra-substituters = [ "https://nix-community.cachix.org" ];
-    extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   inputs = {
@@ -12,39 +14,50 @@
   };
 
   outputs =
-    { self
-    , nixpkgs
-    , nixpkgs-stable
+    {
+      self,
+      nixpkgs,
+      nixpkgs-stable,
     }:
     let
       inherit (nixpkgs) lib;
       forAllSystems = lib.genAttrs lib.systems.flakeExposed;
 
-      importPkgs = path: attrs: import path (attrs // {
-        config.allowAliases = false;
-        overlays = [ self.overlays.default ];
-      });
+      importPkgs =
+        path: attrs:
+        import path (
+          attrs
+          // {
+            config.allowAliases = false;
+            overlays = [ self.overlays.default ];
+          }
+        );
 
-      packages' = forAllSystems (system: (
-        let
-          pkgs = importPkgs nixpkgs { inherit system; };
-          inherit (pkgs) lib;
+      packages' = forAllSystems (
+        system:
+        (
+          let
+            pkgs = importPkgs nixpkgs { inherit system; };
+            inherit (pkgs) lib;
 
-          overlayAttributes = lib.pipe (import ./. pkgs pkgs) [
-            builtins.attrNames
-            (lib.partition (n: lib.isDerivation pkgs.${n}))
-          ];
-          attributesToAttrset = attributes: lib.pipe attributes [
-            (map (n: lib.nameValuePair n pkgs.${n}))
-            lib.listToAttrs
-          ];
+            overlayAttributes = lib.pipe (import ./. pkgs pkgs) [
+              builtins.attrNames
+              (lib.partition (n: lib.isDerivation pkgs.${n}))
+            ];
+            attributesToAttrset =
+              attributes:
+              lib.pipe attributes [
+                (map (n: lib.nameValuePair n pkgs.${n}))
+                lib.listToAttrs
+              ];
 
-        in
-        {
-          lib = attributesToAttrset overlayAttributes.wrong;
-          packages = attributesToAttrset overlayAttributes.right;
-        }
-      ));
+          in
+          {
+            lib = attributesToAttrset overlayAttributes.wrong;
+            packages = attributesToAttrset overlayAttributes.right;
+          }
+        )
+      );
 
     in
     {
@@ -57,24 +70,28 @@
       # for backward compatibility, is safe to delete, not referenced anywhere
       overlay = self.overlays.default;
 
-      hydraJobs =
-        lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (system:
+      hydraJobs = lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (
+        system:
         let
-          mkHydraJobs = pkgs:
+          mkHydraJobs =
+            pkgs:
             let
               inherit (pkgs) lib;
 
-              filterNonDrvAttrs = s: lib.mapAttrs (_: v: if (lib.isDerivation v) then v else filterNonDrvAttrs v) (lib.filterAttrs (_: v: lib.isDerivation v || (builtins.typeOf v == "set" && ! builtins.hasAttr "__functor" v)) s);
+              filterNonDrvAttrs =
+                s:
+                lib.mapAttrs (_: v: if (lib.isDerivation v) then v else filterNonDrvAttrs v) (
+                  lib.filterAttrs (
+                    _: v: lib.isDerivation v || (builtins.typeOf v == "set" && !builtins.hasAttr "__functor" v)
+                  ) s
+                );
 
               mkEmacsSet = emacs: filterNonDrvAttrs (pkgs.recurseIntoAttrs (pkgs.emacsPackagesFor emacs));
 
             in
             {
               emacsen = {
-                inherit (pkgs) emacs-unstable emacs-unstable-nox;
-                inherit (pkgs) emacs-unstable-pgtk;
-                inherit (pkgs) emacs-git emacs-git-nox;
-                inherit (pkgs) emacs-git-pgtk;
+                inherit (pkgs) emacs-git emacs-git-pgtk;
                 inherit (pkgs) emacs-igc emacs-igc-pgtk;
               };
             };
@@ -83,7 +100,8 @@
         {
           "stable" = mkHydraJobs (importPkgs nixpkgs-stable { inherit system; });
           "unstable" = mkHydraJobs (importPkgs nixpkgs { inherit system; });
-        });
+        }
+      );
 
       packages = forAllSystems (system: packages'.${system}.packages);
       lib = forAllSystems (system: packages'.${system}.lib);
